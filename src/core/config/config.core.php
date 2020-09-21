cf32283f92b8ca18c64a2638429cb1694c5cf8df
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2020-09-21 12:42:30 +0300

    Added data for databes connection and some others

--- src/core/config/config.core.php
+++ src/core/config/config.core.php
@@ -5,3 +5,15 @@
  *
  * Подключение к базе данных, определение констант
  */
+
+$db_type = 'mysql';
+$db_server = '127.0.0.1';
+$db_user = 'root';
+$db_password = 'root';
+$db_connection_charset = 'utf8';
+$dbase = 'slepCMS';
+$table_prefix = 'slep_';
+$db_dsn = 'mysql:host=' . $db_server . ';dbname=' . $dbase . ';charset=' . $db_connection_charset;
+
+!defined('SLEP_CONTROLLER_PATH') && define('SLEP_CONTROLLER_PATH', SLEP_CORE_PATH . '/Controller/');
+!defined('SLEP_MODEL_PATH') && define('SLEP_MODEL_PATH', SLEP_CORE_PATH . '/Model/');
