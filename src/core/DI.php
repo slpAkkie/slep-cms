3ceb36653d539b7c241ff384026ff8d09dda4c5d
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2020-09-25 23:39:13 +0300

    Dependencies Injection class

---
+++ src/core/DI.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Этот файл является компонентном slepCMS.
+ *
+ * @author Shamanin Alexander
+ * @version 0.0.2
+ */
+
+namespace App\Helper;
+
+/**
+ * Класс Dependencies Injection
+ *
+ * Хранит и дает доступ к зависимостям необходимые для работы приложения
+ */
+class DI {
+
+  /**
+   * Ассоциативный массив всех зависимостей
+   */
+  private $container = array();
+
+
+  /**
+   * Получить зависимость по ключу
+   *
+   * @param string $key Имя запрашиваемой зависимости
+   *
+   * @return any - Возвращает зависимость любого типа или null если такой зависимости нет
+   */
+  public function get($key) {
+    return $this->isset($key) ? $this->$container[$key] : null;
+  }
+
+
+  /**
+   * Установить зависимость по ключу
+   *
+   * @param string $key Имя зависимости
+   * @param any $value Значение зависимости
+   *
+   * @return Возвращает экземляр класса DI
+   */
+  public function set($key, $value) {
+    $this->$list[$key] = $value;
+
+    return $this;
+  }
+
+
+  /**
+   * Проверяет есть ли такая зависимость
+   *
+   * @param string $key Имя зависимости
+   *
+   * @return bool
+   */
+  private function isset($key) {
+    return $this->$container[$key] ? true : false;
+  }
+
+
+}
