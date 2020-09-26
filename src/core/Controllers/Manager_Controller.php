d82cc8baee42367371f756ddea6400e78b6c0990
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2020-09-26 20:04:58 +0300

    Added new controller

---
+++ src/core/Controllers/Manager_Controller.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Этот файл является компонентном slepCMS.
+ *
+ * @author Shamanin Alexander
+ * @version 0.0.1
+ */
+
+namespace Controllers;
+
+
+
+
+
+class ManagerController extends BaseController { }
